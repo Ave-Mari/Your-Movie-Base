d8290059ba7978dd25565a9db6730ab8b1b4a8c7
Mari Vaypan <avemari98@gmail.com>
2023-02-05 21:21:29 +0300

    add router to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+//router
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//components
+import Dashboard from './components/Dashboard.jsx'
+import Preferences from './components/Preferences.jsx'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className='wrapper'>
+        <Switch> 
+            <Route path="/dashboard">
+                <Dashboard />
+            </Route>
+            <Route path="/references">
+                <Preferences />
+            </Route>
+        </Switch>
+    </div>
   )
 }
