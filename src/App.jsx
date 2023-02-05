ab1860330db56e115e83e84589c5ac159128da40
Mari Vaypan <avemari98@gmail.com>
2023-02-05 21:42:40 +0300

    fix the routes and add login component to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 //router
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //components
 import Dashboard from './components/Dashboard.jsx'
 import Preferences from './components/Preferences.jsx'
+import Login from './components/Login.jsx'
 
 
 export default function App() {
-  return (
-    <div className='wrapper'>
-        <Switch> 
-            <Route path="/dashboard">
-                <Dashboard />
-            </Route>
-            <Route path="/references">
-                <Preferences />
-            </Route>
-        </Switch>
-    </div>
-  )
+    const [token, setToken] = useState();
+
+    if (!token) {
+        return <Login setToken={setToken} />
+    }
+
+    return (
+        <div className='wrapper'>
+            <h1>App</h1>
+            <Routes> 
+                <Route path="/dashboard" element={ <Dashboard />} />
+                <Route path="/preferences" element={ <Preferences />} />
+            </Routes>
+        </div>
+    )
 }
