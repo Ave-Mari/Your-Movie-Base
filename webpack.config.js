ac4f873b62c4b40047884567575d0b9d276b0466
Mari Vaypan <avemari98@gmail.com>
2023-02-05 21:21:19 +0300

    change document title

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>"Your Movie Database"</title><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><div id=\"app\"></div></body></html>',
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Your Movie Database</title><meta name="viewport" content="width=device-width, initial-scale=1"></head><body><div id=\"app\"></div></body></html>',
       filename: 'index.html',
     })
   ],
