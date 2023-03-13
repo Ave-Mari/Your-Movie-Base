3c6d57fdee03c2b38c5ee9f636575cebdbe672c9
Mari Vaypan <avemari98@gmail.com>
2023-03-13 18:55:17 +0300

    add outlet element

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Popular from './../Popular/Popular.jsx'
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function Dashboard() {  
   return (
     <section>
       <Link to="popular">Popular</Link>
+      <Outlet />
     </section>
   )
 }
