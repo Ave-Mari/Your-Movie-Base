2fb64023dd374e57538bda47ea4cfd0066b77ff3
Mari Vaypan <avemari98@gmail.com>
2023-03-14 00:27:02 +0300

    add basic style to popular component

---
+++ src/components/Popular/Popular.styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+
+    ul {
+        display: flex;
+    }
+
+    li {
+        background: #CFBC97;
+        border-radius: 25px;
+        list-style: none;
+
+        h2 {
+            font-size: 22px;
+        }
+        img {
+            width: 250px;
+        }
+    }
+`;
