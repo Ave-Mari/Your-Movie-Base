3f9762bd40535539e9975fed0e1e645ff81e77d6
Mari Vaypan <avemari98@gmail.com>
2023-03-15 00:33:33 +0300

    add basic style to movie card

--- src/components/Popular/Popular.styles.js
+++ src/components/Popular/Popular.styles.js
@@ -4,6 +4,11 @@ export const Wrapper = styled.section`
 
     ul {
         display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        width: 96%;
+        padding-left: 0px;
+        margin: 0 auto;
     }
 
     li {
@@ -11,11 +16,32 @@ export const Wrapper = styled.section`
         border-radius: 25px;
         list-style: none;
 
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        padding: 22px;
+
+        .title {
+            margin: 0 0 12px 0;
+            text-aligh: center;
+            font-weight: 600;
+            font-size: 16px;
+        }
+
+        .date {
+            margin: 0;
+            text-aligh: center;
+            font-weight: 300;
+            font-size: 15px;
+        }
+
         h2 {
             font-size: 22px;
         }
         img {
-            width: 250px;
+            width: 220px;
+            margin-bottom: 13px;
         }
     }
 `;
