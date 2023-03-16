b1dbabdca7069226fd2701178fa40f21c5e9e558
Mari Vaypan <avemari98@gmail.com>
2023-03-16 19:17:39 +0300

    add boolean value to like state

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import { Wrapper } from './MovieCard.styles.js';
 
-export default function MovieCard( { poster_path, original_title, release_date, id} ) {
+export default function MovieCard( { poster_path, original_title, release_date, id, likeSwitcher } ) {
+
+  const [like, setLike] = useState(false);
   return (
-    <Wrapper key={id}>
-        <button>❤</button>
+    <Wrapper key={id} like={like}>
+        <button onClick={() => {
+          setLike(!like); 
+          likeSwitcher(id, like);
+          }}
+          className={like ? 'like' : null}
+          >
+            ❤
+          </button>
         <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={original_title} />
         <p class="title">{original_title}</p>
         <p class="date">{release_date}</p>
