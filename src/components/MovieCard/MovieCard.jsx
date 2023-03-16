bd64845702859f07b1145dc552906c347f7890f4
Mari Vaypan <avemari98@gmail.com>
2023-03-16 17:50:58 +0300

    add like botton and styles to it

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,6 +4,7 @@ import { Wrapper } from './MovieCard.styles.js';
 export default function MovieCard( { poster_path, original_title, release_date, id} ) {
   return (
     <Wrapper key={id}>
+        <button>❤</button>
         <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={original_title} />
         <p class="title">{original_title}</p>
         <p class="date">{release_date}</p>
--- src/components/MovieCard/MovieCard.styles.js
+++ src/components/MovieCard/MovieCard.styles.js
@@ -2,14 +2,17 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.li`
 
-    // li {
+  
         background: #CFBC97;
         border-radius: 25px;
         list-style: none;
+        position: relative;
 
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: space-between;
+        margin-bottom: 22px;
 
         padding: 22px;
 
@@ -18,6 +21,9 @@ export const Wrapper = styled.li`
             text-aligh: center;
             font-weight: 600;
             font-size: 16px;
+
+            width: 13.9vw;
+            text-align: center;
         }
 
         .date {
@@ -34,5 +40,22 @@ export const Wrapper = styled.li`
             width: 220px;
             margin-bottom: 13px;
         }
-    // }
+
+        button {
+            border: none;
+            background: none;
+            font-size: 31px;
+            color: rgb(234, 224, 239);
+            text-shadow: #AB1834 3px 0px 5px;
+            position: absolute;
+            top: 2%;
+            right: 3%;
+
+            &:hover {
+                color: rgb(239, 178, 227);
+                cursor: pointer;
+            }
+
+        }
+    
 `;
