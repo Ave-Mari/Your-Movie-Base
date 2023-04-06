fa641ed10d53941219c8a649fbd9470aa8a65758
Mari Vaypan <avemari98@gmail.com>
2023-04-06 21:41:00 +0300

    map array of users' famovite movies

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react';
+//components
+import MovieCard from './../MovieCard/MovieCard.jsx'
 
-export default function MyList() {
+export default function MyList( { userFavList } ) {
   return (
     <section>
        <h2>My Favorite Movies</h2> 
+       <div className="wrapper">
+        {userFavList.map((item) => {
+          const {original_title, release_date, poster_path, id, like} = item;
+          return (
+            <MovieCard
+            id={id}
+            original_title={original_title}
+            poster_path={poster_path}
+            release_date={release_date}
+        />
+          )
+        })}
+       </div>
     </section>
   )
 }
