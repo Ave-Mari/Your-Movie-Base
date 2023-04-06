a06ad729dedaaebfa7afcf178a01f40102894f33
Mari Vaypan <avemari98@gmail.com>
2023-04-06 21:40:31 +0300

    add function tp movie card component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Wrapper } from './MovieCard.styles.js';
 
-export default function MovieCard( { poster_path, original_title, release_date, id, likeSwitcher, like } ) {
+export default function MovieCard( { addToFav, poster_path, original_title, release_date, id, likeSwitcher, like } ) {
  
   return (
     <Wrapper key={id} like={like}>
         <button 
           onClick={() => {
           likeSwitcher(id);
+          addToFav();
           }}
           className={like ? 'like' : 'no-like'}
           >
